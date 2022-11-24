f8c5574a5cea6a85688f83725ffb2857e81d50ad
seumsims <seumworldinbangladesh@gmail.com>
2022-11-24 10:07:10 +0600

    master side menu completed

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+
+
+// Logs Message
+gulp.task('message', function(done) {
+    console.log("HTTP Server Started");
+    done();
+  });
+
+  gulp.task('default', function(done) {
+    console.log("HTTP Server Started");
+    done();
+  });
